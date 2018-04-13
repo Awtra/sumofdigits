424c87c2c2e89bdc63c4b405479cbc8e6007c425
Ali <aliimody1@gmail.com>
2018-04-13 08:25:32 -0400

    Add files via upload

---
+++ Mody_Digits.py
@@ -0,0 +1,20 @@
+#Ali Mody
+#This program asks the user to enter a series of single digit numbers with nothing
+#separating them. It should then display a series of single-digit
+#numbers in a string.
+
+#Initialize the Variables
+
+digits = ""
+sumnum = ""
+i = 0
+plus = 0
+
+#Asks the user to input the single-digit numbers
+digits = str(input("Please enter the single-digits   "))
+
+#Calculate the sum
+
+
+
+
